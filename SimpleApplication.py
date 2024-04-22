3967e2950694fc9baedaae28e4025bab19133122
JinYuan-alt <232499R@mymail.nyp.edu.sg>
2024-04-22 14:44:59 +0800

    Initial commit

---
+++ SimpleApplication.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/') #controller- controls where it is redirected#
+def layout():
+    return render_template('layout.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run()
